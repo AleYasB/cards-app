c41d4e51474d81c06c994a9556df535ee87b946e
AleYasB <alejandraysb_4897@hotmail.com>
2023-05-26 19:35:19 -0500

    primer commit cards

---
+++ src/App.js
@@ -0,0 +1,16 @@
+/*import logo from "./logo.svg";*/
+import "./App.css";
+/*import Card from "./Componenets/Card";*/
+import Cards from "./Componenets/Cards";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Cards></Cards>
+    </div>
+  );
+}
+
+export default App;
